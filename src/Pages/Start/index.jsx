94a0f95c2ba56a7f7be96246b18b0697c0990d6a
Caduh Mourão <caduh.dev@gmail.com>
2022-11-28 21:16:26 -0300

    Inserindo animação e estruturando componentes

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ScrollView, View, Text, Image, StyleSheet } from 'react-native';
 
+import LifeStatus from '../../Components/Common/LifeStatus';
+
 export default function Start() {
     return (
         <View style={styles.container}>
@@ -8,6 +10,9 @@ export default function Start() {
                 <View style={{alignItems: "center"}}>
                     <Image
                         source={require("../../assets/icons/logo3.png")} style={styles.logo} />
+                    
+                    <LifeStatus />
+
                     <Text style={styles.description}>
                         Vamos transformar sua vida {"\n"} em jogo . buscando sempre {"\n"} o melhor nível.
                     </Text>
